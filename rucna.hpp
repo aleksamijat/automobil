6298c13c84b88b9ca97cbf958d732a0bb45def8c
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 22:00:32 +0200

    final update

--- rucna.hpp
+++ rucna.hpp
@@ -7,33 +7,48 @@ enum RucnaStanje {ANGAZOVANA, NE_ANGAZOVANA, NE_ISPRAVNA_RUCNA};
 class Rucna : public Fixable
 {
 private:
-    RucnaStanje stanje;
+        RucnaStanje stanje;
+        bool isMehanicka;
 public:
-    Rucna()
-    {
-        stanje = ANGAZOVANA;
-    }
-    Rucna(RucnaStanje stanje_)
-    {
-        stanje = stanje_;
-    }
-    Rucna(const Rucna &copy_rucna)
-    {
-        stanje = copy_rucna.stanje;
-    }
-    RucnaStanje getStanje()
-    {
-        return stanje;
-    }
-    void setStanje(RucnaStanje stanje_)
-    {
-        stanje = stanje_;
-    }
-    void fix()override{
-        if(stanje == NE_ISPRAVNA_RUCNA){
-            stanje = NE_ANGAZOVANA;
+        Rucna()
+        {
+                stanje = ANGAZOVANA;
+                isMehanicka = true;
         }
-    }
+        Rucna(RucnaStanje stanje_, bool isMehanicka)
+        {
+                stanje = stanje_;
+                this -> isMehanicka = isMehanicka;
+        }
+        Rucna(const Rucna &copy_rucna)
+        {
+                stanje = copy_rucna.stanje;
+                isMehanicka = copy_rucna.isMehanicka;
+        }
+
+        RucnaStanje getStanje()
+        {
+                return stanje;
+        }
+        bool getIsMehanicka()
+        {
+                return isMehanicka;
+        }
+
+        void setStanje(RucnaStanje stanje_)
+        {
+                stanje = stanje_;
+        }
+        void setIsmehanicka(bool isMehanicka)
+        {
+                this -> isMehanicka = isMehanicka;
+        }
+
+        void fix()override{
+                if(stanje == NE_ISPRAVNA_RUCNA){
+                stanje = NE_ANGAZOVANA;
+                }
+        }
 };
 
 #endif // RUCNA_HPP_INCLUDED
