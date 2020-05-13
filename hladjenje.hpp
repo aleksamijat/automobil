af750aa147732b648880daf0f1b9c229eac1edea
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 21:56:04 +0200

    final update

--- hladjenje.hpp
+++ hladjenje.hpp
@@ -5,48 +5,48 @@
 class Hladjenje : public Fixable
 {
 private:
-    bool isIspravno;
-    bool isVodeno;
+        bool isIspravno;
+        bool isVodeno;
 public:
-    Hladjenje()
-    {
-        isIspravno = true;
-        isVodeno = true;
-    }
-    Hladjenje(bool isIspravno, bool isVodeno)
-    {
-        this -> isIspravno = isIspravno;
-        this -> isVodeno = isVodeno;
-    }
-    Hladjenje(const Hladjenje &copy_hladjenje)
-    {
-        isIspravno = copy_hladjenje.isIspravno;
-        isVodeno = copy_hladjenje.isVodeno;
-    }
-
-    bool getIsIspravno()
-    {
-        return isIspravno;
-    }
-    bool getIsVodeno()
-    {
-        return isVodeno;
-    }
-
-    void setIsIspravno(bool isIspravno)
-    {
-        this -> isIspravno = isIspravno;
-    }
-    void setIsVodeno(bool isVodeno)
-    {
-        this -> isVodeno = isVodeno;
-    }
-
-    void fix()override{
-        if(isIspravno == false){
-            isIspravno == true;
+        Hladjenje()
+        {
+            isIspravno = true;
+            isVodeno = true;
         }
-    }
+        Hladjenje(bool isIspravno, bool isVodeno)
+        {
+            this -> isIspravno = isIspravno;
+            this -> isVodeno = isVodeno;
+        }
+        Hladjenje(const Hladjenje &copy_hladjenje)
+        {
+            isIspravno = copy_hladjenje.isIspravno;
+            isVodeno = copy_hladjenje.isVodeno;
+        }
+    
+        bool getIsIspravno()
+        {
+            return isIspravno;
+        }
+        bool getIsVodeno()
+        {
+            return isVodeno;
+        }   
+    
+        void setIsIspravno(bool isIspravno)
+        {
+            this -> isIspravno = isIspravno;
+        }
+        void setIsVodeno(bool isVodeno)
+        {
+            this -> isVodeno = isVodeno;
+        }   
+    
+        void fix()override{
+            if(isIspravno == false){
+                isIspravno == true;
+            }
+        }
 };
 
 #endif // HLADJENJE_H_INCLUDED
