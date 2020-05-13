9d86d20a4235981a7d0a3ba571ba3a48c7acf55f
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 21:58:58 +0200

    final update

--- ozvucenje.hpp
+++ ozvucenje.hpp
@@ -7,33 +7,46 @@ enum OzvucenjeStanje {UKLJUCENO, ISKLJUCENO, NE_ISPRAVNO};
 class Ozvucenje : public Fixable
 {
 private:
-    OzvucenjeStanje stanje;
+        OzvucenjeStanje stanje;
+        int jacina;
 public:
-    Ozvucenje()
-    {
-        stanje = ISKLJUCENO;
-    }
-    Ozvucenje(OzvucenjeStanje stanje_)
-    {
-        stanje = stanje_;
-    }
-    Ozvucenje(const Ozvucenje &copy_ozvucenje)
-    {
-        stanje = copy_ozvucenje.stanje;
-    }
-    OzvucenjeStanje getStanje()
-    {
-        return stanje;
-    }
-    void setStanje(OzvucenjeStanje stanje_)
-    {
-        stanje = stanje_;
-    }
-    void fix()override{
-        if(stanje == NE_ISPRAVNO){
-            stanje = ISKLJUCENO;
+        Ozvucenje()
+        {
+                stanje = ISKLJUCENO;
+                jacina = 200;
         }
-    }
+        Ozvucenje(OzvucenjeStanje stanje_, int jacina_)
+        {
+                stanje = stanje_;
+        }
+        Ozvucenje(const Ozvucenje &copy_ozvucenje)
+        {
+                stanje = copy_ozvucenje.stanje;
+        }
+
+        OzvucenjeStanje getStanje()
+        {
+                return stanje;
+        }
+        int getJacina()
+        {
+                return jacina;
+        }
+
+        void setStanje(OzvucenjeStanje stanje_)
+        {
+                stanje = stanje_;
+        }
+        void setJacina(int jacina_)
+        {
+                jacina = jacina_;
+        }
+
+        void fix()override
+        {
+                if(stanje == NE_ISPRAVNO)
+                stanje = ISKLJUCENO;
+        }
 };
 
 #endif // OZVUCENJE_H_INCLUDED
