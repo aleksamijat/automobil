ae6dcad74de19e25150ae85f3ef49175810a470e
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 21:58:30 +0200

    final update

--- motor.hpp
+++ motor.hpp
@@ -8,75 +8,135 @@ enum MotorStanje {UGASEN, UPALJEN, NE_ISPRAVAN};
 class Motor : public Fixable
 {
 private:
-    TipMotora tip;
-    MotorStanje stanje;
-    int brojCilindara;
+        TipMotora tip;
+        MotorStanje stanje;
+        int brojCilindara;
+        bool isTransverse;
+        int konjskeSnage;
+        int obrtniMoment;
+        float potrosnja;
+        bool isNaturallyAspirated;
 public:
-    Motor()
-    {
-        tip = I;
-        stanje = UGASEN;
-        brojCilindara = 4;
-    }
-    Motor(TipMotora tip_, MotorStanje stanje_, int broj_c)
-    {
-        tip = tip_;
-        stanje = stanje_;
-        brojCilindara = broj_c;
-    }
-    Motor(const Motor &copy_motor)
-    {
-        tip = copy_motor.tip;
-        stanje = copy_motor.stanje;
-        brojCilindara = copy_motor.brojCilindara;
-    }
-    TipMotora getTip()
-    {
-        return tip;
-    }
-    MotorStanje getStanje()
-    {
-        return stanje;
-    }
-    int getCilindara()
-    {
-        return brojCilindara;
-    }
-    void setTip(TipMotora tip_)
-    {
-        tip = tip_;
-    }
-    void setStanje(MotorStanje stanje_)
-    {
-        stanje = stanje_;
-    }
-    void setCilindara(int broj_c)
-    {
-        brojCilindara = broj_c;
-    }
-    void fix()override{
-        if(stanje == NE_ISPRAVAN){
-            stanje = UGASEN;
+        Motor()
+        {
+                tip = I;
+                stanje = UGASEN;
+                brojCilindara = 4;
+                isTransverse = true;
+                konjskeSnage = 44;
+                obrtniMoment = 39;
+                potrosnja = 6.2;
+                isNaturallyAspirated = true;
         }
-    }
-    Motor& operator++(){
-        brojCilindara++;
-        return *this;
+        Motor(TipMotora tip_, MotorStanje stanje_, int broj_c, bool isTransverse,
+              int konjske_s, int obrtni_m, int potrosnja_, bool isNaturallyAspirated)
+        {
+                tip = tip_;
+                stanje = stanje_;
+                brojCilindara = broj_c;
+                this -> isTransverse = isTransverse;
+                konjskeSnage = konjske_s;
+                obrtniMoment = obrtni_m;
+                potrosnja = potrosnja_;
+                this -> isNaturallyAspirated = isNaturallyAspirated;
         }
-    Motor& operator--(){
-        brojCilindara--;
-        return *this;
+        Motor(const Motor &copy_motor)
+        {
+                tip = copy_motor.tip;
+                stanje = copy_motor.stanje;
+                brojCilindara = copy_motor.brojCilindara;
+                isTransverse = copy_motor.isTransverse;
+                konjskeSnage = copy_motor.konjskeSnage;
+                obrtniMoment = copy_motor.obrtniMoment;
+                potrosnja = copy_motor.potrosnja;
+                isNaturallyAspirated = copy_motor.isNaturallyAspirated;
         }
+
+        TipMotora getTip()
+        {
+                return tip;
+        }
+        MotorStanje getStanje()
+        {
+                return stanje;
+        }
+        int getCilindara()
+        {
+                return brojCilindara;
+        }
+        bool getIsTransverse()
+        {
+                return isTransverse;
+        }
+        int getKonjskeSnage()
+        {
+                return konjskeSnage;
+        }
+        int getObrtniMoment()
+        {
+                return obrtniMoment;
+        }
+        float getPotrosnja()
+        {
+                return potrosnja;
+        }
+        bool getIsNaturallyAspirated()
+        {
+                return isNaturallyAspirated;
+        }
+
+        void setTip(TipMotora tip_)
+        {
+                tip = tip_;
+        }
+        void setStanje(MotorStanje stanje_)
+        {
+                stanje = stanje_;
+        }
+        void setCilindara(int broj_c)
+        {
+                brojCilindara = broj_c;
+        }
+        void setIsTransverse(bool isTransverse)
+        {
+                this -> isTransverse = isTransverse;
+        }
+        void setKonjskeSnage(int konjske_s)
+        {
+                konjskeSnage = konjske_s;
+        }
+        void setObrtniMoment(int obrtni_m)
+        {
+                obrtniMoment = obrtni_m;
+        }
+        void setPotrosnja(int potrosnja_)
+        {
+                potrosnja = potrosnja_;
+        }
+        void setIsNaturallyAspirated(bool isNaturallyAspirated)
+        {
+                this -> isNaturallyAspirated = isNaturallyAspirated;
+        }
+
+        void fix()override
+        {
+                if(stanje == NE_ISPRAVAN)
+                stanje = UGASEN;
+        }
+
+        Motor& operator++(){
+                brojCilindara++;
+                return *this;
+                }
+        Motor& operator--(){
+                brojCilindara--;
+                return *this;
+                }
 };
 
 std::ostream& operator<<(std::ostream& o, const Motor& obj)
 {
-    return o;
-}
-std::istream& operator>>(std::istream& i, Motor& obj)
-{
-    i.setstate(std::ios::failbit);
-    return i;
+        return o;
 }
 
 #endif // MOTOR_HPP_INCLUDED
