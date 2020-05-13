c781fbf7b641359600fcbf024597e2cd90f259a3
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 21:58:06 +0200

    final update

--- menjac.hpp
+++ menjac.hpp
@@ -5,64 +5,69 @@
 class Menjac : public Fixable
 {
 private:
-    bool isManual;
-    bool isIspravan;
-    int brojBrzina;
+        bool isManual;
+        bool isIspravan;
+        int brojBrzina;
 public:
-    Menjac()
-    {
-        isManual = true;
-        isIspravan = true;
-        brojBrzina = 4;
-    }
-    Menjac(bool isManual, bool isIspravan, int broj_b)
-    {
-        this -> isManual = isManual;
-        this -> isIspravan = isIspravan;
-        brojBrzina = broj_b;
-    }
-    Menjac(const Menjac &copy_menjac)
-    {
-        isManual = copy_menjac.isManual;
-        isIspravan = copy_menjac.isIspravan;
-        brojBrzina = copy_menjac.brojBrzina;
-    }
-    bool getIsAutomatik()
-    {
-        return isManual;
-    }
-    bool getIsIspravan()
-    {
-        return isIspravan;
-    }
-    int getBrzina()
-    {
-        return brojBrzina;
-    }
-    void setIsAutomatik(bool isManual)
-    {
-        this -> isManual = isManual;
-    }
-    void setIsIspravan(bool isIspravan)
-    {
-        this -> isIspravan = isIspravan;
-    }
-    void setBrzina(int broj_b)
-    {
-        brojBrzina = broj_b;
-    }
-    void fix()override{
-        if(isIspravan == false){
-            isIspravan == true;
+        Menjac()
+        {
+                isManual = true;
+                isIspravan = true;
+                brojBrzina = 4;
         }
-    }
-    Menjac& operator++(){
-        brojBrzina++;
-        return *this;
+        Menjac(bool isManual, bool isIspravan, int broj_b)
+        {
+                this -> isManual = isManual;
+                this -> isIspravan = isIspravan;
+                brojBrzina = broj_b;
         }
-    Menjac& operator--(){
-        brojBrzina--;
-        return *this;
+        Menjac(const Menjac &copy_menjac)
+        {
+                isManual = copy_menjac.isManual;
+                isIspravan = copy_menjac.isIspravan;
+                brojBrzina = copy_menjac.brojBrzina;
+        }
+
+        bool getIsAutomatik()
+        {
+                return isManual;
+        }
+        bool getIsIspravan()
+        {
+                return isIspravan;
+        }
+        int getBrzina()
+        {
+                return brojBrzina;
+        }
+
+        void setIsAutomatik(bool isManual)
+        {
+                this -> isManual = isManual;
+        }
+        void setIsIspravan(bool isIspravan)
+        {
+                this -> isIspravan = isIspravan;
+        }
+        void setBrzina(int broj_b)
+        {
+                brojBrzina = broj_b;
+        }
+
+        void fix()override
+        {
+                if(isIspravan == false);
+                isIspravan == true;
+        }
+        Menjac& operator++()
+        {
+                brojBrzina++;
+                return *this;
+        }
+        Menjac& operator--()
+        {
+                brojBrzina--;
+                return *this;
         }
 };
 
@@ -70,8 +75,9 @@ std::ostream& operator<<(std::ostream& o, const Menjac& obj)
 {
     return o;
 }
-std::istream& operator>>(std::istream& i, Menjac& obj)
-{
+
+#endif // MENJAC_HPP_INCLUDED
+
     i.setstate(std::ios::failbit);
     return i;
 }
