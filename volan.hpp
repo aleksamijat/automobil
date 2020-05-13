3824cf691ae411c56f9f5fbee82a93b734fdee6a
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 22:01:39 +0200

    final update

--- volan.hpp
+++ volan.hpp
@@ -1,33 +1,47 @@
-#ifndef VOLAN_H_INCLUDED
-#define VOLAN_H_INCLUDED
+#ifndef VOLAN_HPP_INCLUDED
+#define VOLAN_HPP_INCLUDED
 
 enum TipVolana {PLASTICNI, SPORTSKI, KOZNI};
 
 class Volan
 {
 private:
-    TipVolana tip;
+        TipVolana tip;
+        int sirina;
 public:
-    Volan()
-    {
-        tip = PLASTICNI;
-    }
-    Volan(TipVolana tip_)
-    {
+        Volan()
+        {
+                tip = PLASTICNI;
+                sirina = 35;
+        }
+        Volan(TipVolana tip_, int sirina_)
+        {
         tip = tip_;
-    }
-    Volan(const Volan &copy_volan)
-    {
-        tip = copy_volan.tip;
-    }
-    TipVolana getTip()
-    {
-        return tip;
-    }
-    void setTip(TipVolana tip_)
-    {
-        tip = tip_;
-    }
+        sirina = sirina_;
+        }
+        Volan(const Volan &copy_volan)
+        {
+                tip = copy_volan.tip;
+                sirina = copy_volan.sirina;
+        }
+
+        TipVolana getTip()
+        {
+                return tip;
+        }
+        int getSirina()
+        {
+                return sirina;
+        }
+
+        void setTip(TipVolana tip_)
+        {
+                tip = tip_;
+        }
+        void setSirina(int sirina_)
+        {
+                sirina = sirina_;
+        }
 };
 
 #endif // VOLAN_HPP_INCLUDED
