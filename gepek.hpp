26e818d930d93fd2c31ed171e30be2ea619a37c5
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-13 14:09:43 +0200

    dodani parametri

--- gepek.hpp
+++ gepek.hpp
@@ -1,31 +1,72 @@
 #ifndef GEPEK_HPP_INCLUDED
 #define GEPEK_HPP_INCLUDED
+#include "prozori.hpp"
+#include "karoserija.hpp"
 
 class Gepek
 {
 private:
-    bool isZatvoren;
+        bool isZatvoren;
+        int zapremina;
+        int height;
+        int width;
 public:
-    Gepek()
-    {
-        isZatvoren = true;
-    }
-    Gepek(bool isZatvoren)
-    {
-        this -> isZatvoren = isZatvoren;
-    }
-    Gepek (const Gepek &copy_gepek)
-    {
-        isZatvoren = copy_gepek.isZatvoren;
-    }
-    bool getIsZatvoren()
-    {
-        return isZatvoren;
-    }
-    void setIsZatvoren(bool isZatvoren)
-    {
-        this -> isZatvoren = isZatvoren;
-    }
+        Gepek()
+        {
+                isZatvoren = true;
+                zapremina = 300;
+                height = 150;
+                width = 110;
+        }
+        Gepek(bool isZatvoren, int zapremina_, int height_, int width_)
+        {
+                this -> isZatvoren = isZatvoren;
+                zapremina = zapremina_;
+                height = height_;
+                width = width_;
+        }
+        Gepek (const Gepek &copy_gepek)
+        {
+                isZatvoren = copy_gepek.isZatvoren;
+                zapremina = copy_gepek.zapremina;
+                height = copy_gepek.height;
+                width = copy_gepek.width;
+        }
+
+        bool getIsZatvoren()
+        {
+                return isZatvoren;
+        }
+        int getZapremina()
+        {
+                return zapremina;
+        }
+        int getHeight()
+        {
+                return height;
+        }
+        int getWidth()
+        {
+                return width;
+        }
+
+        void setIsZatvoren(bool isZatvoren)
+        {
+                this -> isZatvoren = isZatvoren;
+        }
+        void setZapremina(int zapremina_)
+        {
+                zapremina = zapremina_;
+        }
+        void setHeight(int height_)
+        {
+                height = height_;
+        }
+        void setWidth(int width_)
+        {
+                width = width_;
+        }
+
 };
 
 
