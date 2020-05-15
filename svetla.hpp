f939f911caf3cc39ccc02c90a60857f75aed72be
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-15 15:10:38 +0200

    final update

--- svetla.hpp
+++ svetla.hpp
@@ -8,45 +8,45 @@ enum SvetlaStanje {UPALJENA, UGASENA, NE_ISPRAVNA};
 class Svetla : public Fixable
 {
 private:
-    TipSvetala tip;
-    SvetlaStanje stanje;
+        TipSvetala tip;
+        SvetlaStanje stanje;
 public:
-    Svetla()
-    {
-        tip = HALOGEN;
-        stanje = UGASENA;
-    }
-    Svetla(TipSvetala tip_, SvetlaStanje stanje_)
-    {
-        tip = tip_;
-        stanje = stanje_;
-    }
-    Svetla(const Svetla &copy_svetla)
-    {
-        tip = copy_svetla.tip;
-        stanje = copy_svetla.stanje;
-    }
-    TipSvetala getTip()
-    {
-        return tip;
-    }
-    SvetlaStanje getStanje()
-    {
-        return stanje;
-    }
-    void setTip(TipSvetala tip_)
-    {
-        tip = tip_;
-    }
-    void setStanje(SvetlaStanje stanje_)
-    {
-        stanje = stanje_;
-    }
-    void fix()override{
-        if(stanje == NE_ISPRAVNA){
-            stanje = UGASENA;
+        Svetla()
+        {
+                tip = HALOGEN;
+                stanje = UGASENA;
         }
-    }
+        Svetla(TipSvetala tip_, SvetlaStanje stanje_)
+        {
+                tip = tip_;
+                stanje = stanje_;
+        }
+        Svetla(const Svetla &copy_svetla)
+        {
+                tip = copy_svetla.tip;
+                stanje = copy_svetla.stanje;
+        }
+        TipSvetala getTip()
+        {
+                return tip;
+        }
+        SvetlaStanje getStanje()
+        {
+                return stanje;
+        }
+        void setTip(TipSvetala tip_)
+        {
+                tip = tip_;
+        }
+        void setStanje(SvetlaStanje stanje_)
+        {
+                stanje = stanje_;
+        }
+        void fix()override{
+                if(stanje == NE_ISPRAVNA){
+                stanje = UGASENA;
+                        }
+        }
 };
 
 #endif // SVETLA_HPP_INCLUDED
