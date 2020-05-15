80f5431cd88e555938224e186627375f8d222178
Aleksa Mijatović <62738543+aleksamijat@users.noreply.github.com>
2020-05-15 15:06:40 +0200

    final update

--- prenos.hpp
+++ prenos.hpp
@@ -1,46 +1,49 @@
 #ifndef PRENOS_HPP_INCLUDED
 #define PRENOS_HPP_INCLUDED
+#include "fixable.hpp"
 
 enum TipPrenosa {FWD, RWD, AWD};
 
 class Prenos
 {
 private:
-    TipPrenosa tip;
-    bool isIspravan;
+        TipPrenosa tip;
+        bool isIspravan;
 public:
-    Prenos()
-    {
-        tip = FWD;
-        isIspravan = true;
-    }
-    Prenos(TipPrenosa tip_, bool isIspravan)
-    {
-        tip = tip_;
-        this -> isIspravan = isIspravan;
-    }
-    Prenos(const Prenos &copy_prenos)
-    {
-        tip = copy_prenos.tip;
-        isIspravan = copy_prenos.isIspravan;
+        Prenos()
+        {
+                tip = FWD;
+                isIspravan = true;
+        }
+        Prenos(TipPrenosa tip_, bool isIspravan)
+        {
+                tip = tip_;
+                this -> isIspravan = isIspravan;
+        }
+        Prenos(const Prenos &copy_prenos)
+        {
+                tip = copy_prenos.tip;
+                isIspravan = copy_prenos.isIspravan;
 
-    }
-    TipPrenosa getTip()
-    {
-        return tip;
-    }
-    bool getIsIspravan()
-    {
-        return isIspravan;
-    }
-    void setTip(TipPrenosa tip_)
-    {
-        tip = tip_;
-    }
-    void setIsIspravan(bool isIspravan)
-    {
-        this -> isIspravan = isIspravan;
-    }
+        }
+
+        TipPrenosa getTip()
+        {
+                return tip;
+        }
+        bool getIsIspravan()
+        {
+                return isIspravan;
+        }
+
+        void setTip(TipPrenosa tip_)
+        {
+                tip = tip_;
+        }
+        void setIsIspravan(bool isIspravan)
+        {
+                this -> isIspravan = isIspravan;
+        }
 };
 
 #endif // PRENOS_HPP_INCLUDED
